2ca8bc754cf9e07501ba0c0f6c17b67e55a4853b
Asfak Ahmed <asfakahmed680@gmail.com>
2023-12-25 07:16:38 +0600

    readme updated

---
+++ Any/index.ts
@@ -0,0 +1,10 @@
+// any type
+let typeOfAny: any;
+typeOfAny = 10;
+typeOfAny = "string";
+typeOfAny = true;
+typeOfAny = {};
+typeOfAny = [];
+typeOfAny = function () {};
+typeOfAny = null;
+typeOfAny = undefined;
